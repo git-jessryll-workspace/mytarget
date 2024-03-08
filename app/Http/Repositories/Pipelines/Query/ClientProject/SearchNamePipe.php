b93d639c5aa8b7a430cc8f346d5fb517436b2437
git-jessryll-workspace <jessryll.workspace@gmail.com>
2024-03-08 18:26:02 +0800

    retest the query

--- app/Http/Repositories/Pipelines/Query/ClientProject/SearchNamePipe.php
+++ app/Http/Repositories/Pipelines/Query/ClientProject/SearchNamePipe.php
@@ -5,22 +5,22 @@ namespace App\Http\Repositories\Pipelines\Query\ClientProject;
 use App\Http\Repositories\Pipelines\Query\HandleQueryPipe;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class SearchNamePipe extends HandleQueryPipe
 {
 
     protected function queryBuilder(Builder $query): Builder
     {
-        DB::enableQueryLog();
         $searchName = request('search_query') ?? "";
         if (!empty($searchName)) {
 
-            return $query->where(function (Builder $q) use ($searchName){
+            $query->where(function (Builder $q) use ($searchName){
                 return $q->whereRaw("MATCH (project_name) AGAINST (? IN BOOLEAN MODE)", [$searchName."*"]);
             });
         }
-        $q = DB::getQueryLog();
-        var_dump($q);
+        Log::info('Query: ' . $query->toSql());
+        Log::info('Bindings: ' . implode(', ', $query->getBindings()));
 return $query;
         // if (!empty($searchName)) {
         //     $query->where(function (Builder $q) use ($searchName) {
